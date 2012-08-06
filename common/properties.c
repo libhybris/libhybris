6e3a1fa777c200a6ac0c9f4195199e6b02b34cd2
Carsten Munk <carsten.munk@gmail.com>
2012-08-06 20:31:14 +0200

    Add basics for property handling
    Signed-off-by: Carsten Munk <carsten.munk@gmail.com>

---
+++ common/properties.c
@@ -0,0 +1,22 @@
+int property_get(const char *key, char *value, const char *default_value)
+{  
+ char *ret = NULL; 
+ printf("property_get: %s\n", key);
+ /* default */
+ {  
+    ret = default_value;
+ }  
+ if (ret)
+ {
+  strcpy(value, ret);
+  return strlen(ret);
+ }
+ else
+ {
+  return 0;
+ }
+}
+int property_set(const char *key, const char *value)
+{
+   printf("property_set: %s %s\n", key, value);
+}
