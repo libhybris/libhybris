f2f1bbfcb61cd4b25f42d8929cc6216a097828d1
Carsten Munk <carsten.munk@jollamobile.com>
2012-09-29 08:54:30 +0200

    Add some more hooks
    Signed-off-by: Carsten Munk <carsten.munk@jollamobile.com>

--- common/hooks.c
+++ common/hooks.c
@@ -9,6 +9,7 @@
 #include <strings.h>
 #include <dlfcn.h>
 #include <pthread.h>
+#include <errno.h>
 
 struct _hook {
   const char *name;
@@ -122,6 +123,41 @@ static int my_pthread_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mute
   return pthread_cond_timedwait(realcond, realmutex, abstime);    
 }
 
+static int my_pthread_attr_destroy(pthread_attr_t *__attr)
+{
+  pthread_attr_t *realattr = (pthread_attr_t *) *(int *) __attr;
+  return pthread_attr_destroy(realattr);
+}
+
+static int my_pthread_attr_setdetachstate(pthread_attr_t *__attr, int detachstate)
+{
+  pthread_attr_t *realattr = (pthread_attr_t *) *(int *) __attr;
+  return pthread_attr_setdetachstate(realattr, detachstate);
+}
+
+
+static int my_pthread_attr_setstacksize(pthread_attr_t *__attr, size_t stacksize)
+{
+
+  pthread_attr_t *realattr = (pthread_attr_t *) *(int *) __attr;
+  return pthread_attr_setstacksize (realattr,stacksize);
+}
+
+static int my_pthread_attr_init(pthread_attr_t *__attr) 
+{
+  pthread_attr_t *realattr = (pthread_attr_t *) *(int *) __attr;
+
+  realattr = malloc(sizeof(pthread_attr_t));
+  *((int *)__attr) = (int) realattr;
+
+  return pthread_attr_init(realattr);
+}
+
+static int my_set_errno(int oi_errno)
+{
+    errno = oi_errno;
+    return -1;
+}
 
 static struct _hook hooks[] = {
   {"property_get", property_get },
@@ -199,8 +235,10 @@ static struct _hook hooks[] = {
   {"pthread_once", pthread_once},
   {"pthread_mutexattr_init", pthread_mutexattr_init},
   {"pthread_mutexattr_settype", pthread_mutexattr_settype},
+  {"pthread_mutexattr_destroy", pthread_mutexattr_destroy},
   {"pthread_mutex_trylock", my_pthread_mutex_trylock},   
   {"pthread_key_create", pthread_key_create},
+  {"pthread_key_delete", pthread_key_delete},
   {"pthread_setspecific", pthread_setspecific},
   {"pthread_getspecific", pthread_getspecific},
   {"pthread_cond_init", my_pthread_cond_init},
@@ -209,10 +247,21 @@ static struct _hook hooks[] = {
   {"pthread_cond_signal", my_pthread_cond_signal},
   {"pthread_cond_wait", my_pthread_cond_wait},
   {"pthread_cond_timedwait", my_pthread_cond_timedwait},
+  {"pthread_attr_setstacksize", my_pthread_attr_setstacksize},
+  {"pthread_attr_destroy", my_pthread_attr_destroy},
+  {"pthread_attr_setdetachstate", my_pthread_attr_setdetachstate},
+  {"pthread_attr_init", my_pthread_attr_init},
+  {"pthread_rwlock_init", pthread_rwlock_init},
+  {"pthread_rwlock_destroy", pthread_rwlock_destroy},
+  {"pthread_rwlock_unlock", pthread_rwlock_unlock},
+  {"pthread_rwlock_wrlock", pthread_rwlock_wrlock},
+  {"pthread_rwlock_rdlock", pthread_rwlock_rdlock},
   {"__get_tls", -1},
   {"fopen", fopen},
   {"fgets", fgets},
   {"fclose", fclose},
+  {"__errno", __errno_location},
+  {"__set_errno", my_set_errno},
     {NULL, NULL},
 };
 
