0b5e255bcf9bb577b80ea7499de5a6835ee98940
Simon Busch <morphis@gravedo.de>
2013-03-21 17:14:37 +0100

    Implement binding for Android's libui library through a simple compat library
    Signed-off-by: Simon Busch <morphis@gravedo.de>

---
+++ compat/ui/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+HYBRIS_PATH := $(LOCAL_PATH)/../../hybris
+
+LOCAL_SRC_FILES:= \
+	ui_compatibility_layer.cpp
+
+LOCAL_MODULE:= libui_compat_layer
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_C_INCLUDES := \
+	$(HYBRIS_PATH)/include \
+	frameworks/native/include
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libutils \
+	libbinder \
+	libhardware \
+	libui
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
