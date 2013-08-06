b565bdc6cdb5a72f56b3aa3e9728eec19fa89906
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2013-08-06 20:54:44 -0300

    properties: add support to get/set property via the property service
    The property service is running inside the android init daemon and
    creates a socket in /dev/socket/property_service which we can use
    for setting properties (getting is done via an extra android patch
    to add 'get' and 'list' support in the property service).
    
    Additional patches for the Android init system can be found at
    http://people.canonical.com/~rsalveti/properties/
    
    Based on a similar patch by Simon Busch <morphis@gravedo.de>.
    
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- hybris/properties/properties.c
+++ hybris/properties/properties.c
@@ -1,5 +1,8 @@
 /*
  * Copyright (c) 2012 Carsten Munk <carsten.munk@gmail.com>
+ *               2008 The Android Open Source Project
+ *               2013 Simon Busch <morphis@gravedo.de>
+ *               2013 Canonical Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,10 +25,21 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include "logging.h"
+#define __USE_GNU
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <poll.h>
 
-#define PROP_NAME_MAX 32
+#include <hybris/properties/properties.h>
 
+static const char property_service_socket[] = "/dev/socket/" PROP_SERVICE_NAME;
+
+/* Find a key value from a static /system/build.prop file */
 static char *find_key(const char *key)
 {
 	FILE *f = fopen("/system/build.prop", "r");
@@ -60,6 +74,8 @@ static char *find_key(const char *key)
 	return NULL;
 }
 
+/* Find a key value from the kernel command line, which is parsed
+ * by Android at init (on an Android working system) */
 static char *find_key_kernel_cmdline(const char *key)
 {
 	char cmdline[1024];
@@ -108,62 +124,146 @@ static char *find_key_kernel_cmdline(const char *key)
 	return NULL;
 }
 
+/* Get/Set a property from the Android Init property socket */
+static int send_prop_msg(prop_msg_t *msg,
+		void (*propfn)(const char *, const char *, void *),
+		void *cookie)
+{
+	struct pollfd pollfds[1];
+	union {
+		struct sockaddr_un addr;
+		struct sockaddr addr_g;
+	} addr;
+	socklen_t alen;
+	size_t namelen;
+	int s;
+	int r;
+	int result = -1;
+	int patched_init = 0;
+
+	s = socket(AF_LOCAL, SOCK_STREAM, 0);
+	if (s < 0) {
+		return result;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	namelen = strlen(property_service_socket);
+	strncpy(addr.addr.sun_path, property_service_socket,
+			sizeof(addr.addr.sun_path));
+	addr.addr.sun_family = AF_LOCAL;
+	alen = namelen + offsetof(struct sockaddr_un, sun_path) + 1;
+
+	if (TEMP_FAILURE_RETRY(connect(s, &addr.addr_g, alen) < 0)) {
+		close(s);
+		return result;
+	}
+
+	r = TEMP_FAILURE_RETRY(send(s, msg, sizeof(prop_msg_t), 0));
+
+	if (r == sizeof(prop_msg_t)) {
+		pollfds[0].fd = s;
+		pollfds[0].events = 0;
+		// We successfully wrote to the property server, so use recv
+		// in case we need to get a property. Once the other side is
+		// finished, the socket is closed.
+		while ((r = recv(s, msg, sizeof(prop_msg_t), 0)) > 0) {
+			if (r != sizeof(prop_msg_t)) {
+				close(s);
+				return result;
+			}
+
+			/* If we got a reply, this is a patched init */
+			if (!patched_init)
+				patched_init = 1;
+
+			if (propfn)
+				propfn(msg->name, msg->value, cookie);
+		}
+
+		/* We also just get a close in case of setprop */
+		if ((r >= 0) && (patched_init ||
+				(msg->cmd == PROP_MSG_SETPROP))) {
+			result = 0;
+		}
+	}
+
+	close(s);
+	return result;
+}
+
+int property_list(void (*propfn)(const char *key, const char *value, void *cookie),
+		void *cookie)
+{
+	int err;
+	prop_msg_t msg;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.cmd = PROP_MSG_LISTPROP;
+
+	err = send_prop_msg(&msg, propfn, cookie);
+	if (err < 0) {
+		return err;
+	}
+
+	return 0;
+}
+
+static int property_get_socket(const char *key, char *value, const char *default_value)
+{
+	int err;
+	int len;
+	prop_msg_t msg;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.cmd = PROP_MSG_GETPROP;
+
+	if (key) {
+		strncpy(msg.name, key, sizeof(msg.name));
+		err = send_prop_msg(&msg, NULL, NULL);
+		if (err < 0)
+			return err;
+	}
+
+	/* In case it's null, just use the default */
+	if ((strlen(msg.value) == 0) && (default_value)) {
+		if (strlen(default_value) >= PROP_VALUE_MAX) return -1;
+		len = strlen(default_value);
+		memcpy(msg.value, default_value, len + 1);
+	}
+
+	strncpy(value, msg.value, sizeof(msg.value));
+
+	return 0;
+}
+
 int property_get(const char *key, char *value, const char *default_value)
 {
-	char *ret = NULL; 
+	char *ret = NULL;
 
-	//printf("property_get: %s\n", key);
+	if ((key) && (strlen(key) >= PROP_NAME_MAX)) return -1;
 
-	/* default */
-	ret = find_key(key);
+	if (property_get_socket(key, value, default_value) == 0) {
+		if (value)
+			return strlen(value);
+		else
+			return 0;
+	}
 
-#if 0
- if (strcmp(key, "ro.kernel.qemu") == 0)
- {
-    ret = "0";
- }  
- else if (strcmp(key, "ro.hardware") == 0)
- { 
-    ret = "tenderloin";
- } 
- else if (strcmp(key, "ro.product.board") == 0)
- {
-    ret = "tenderloin";
- }
- else if (strcmp(key, "ro.board.platform") == 0)
- { 
-    ret = "msm8660";
- }
- else if (strcmp(key, "ro.arch") == 0)
- {
-    ret = "armeabi";
- }
- else if (strcmp(key, "debug.composition.type") == 0)
- {
-    ret = "c2d"; 
- }
- else if (strcmp(key, "debug.sf.hw") == 0)
- {
-   ret = "1";
- }
- else if (strcmp(key, "debug.gr.numframebuffers") == 0)
- { 
-   ret = "1"; 
- }  
-#endif
-	if (ret == NULL) {
+	/* In case the socket is not available, parse the file by hand */
+	if ((ret = find_key(key)) == NULL) {
 		/* Property might be available via /proc/cmdline */
 		ret = find_key_kernel_cmdline(key);
 	}
 
 	if (ret) {
-		TRACE("found %s for %s\n", key, ret);
 		strcpy(value, ret);
 		free(ret);
 		return strlen(value);
 	} else if (default_value != NULL) {
 		strcpy(value, default_value);
 		return strlen(value);
+	} else {
+		value = '\0';
 	}
 
 	return 0;
@@ -171,8 +271,24 @@ int property_get(const char *key, char *value, const char *default_value)
 
 int property_set(const char *key, const char *value)
 {
-	printf("property_set: %s %s\n", key, value);
-	TRACE("property_set: %s %s\n", key, value);
+	int err;
+	prop_msg_t msg;
+
+	if (key == 0) return -1;
+	if (value == 0) value = "";
+	if (strlen(key) >= PROP_NAME_MAX) return -1;
+	if (strlen(value) >= PROP_VALUE_MAX) return -1;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.cmd = PROP_MSG_SETPROP;
+	strncpy(msg.name, key, sizeof(msg.name));
+	strncpy(msg.value, value, sizeof(msg.value));
+
+	err = send_prop_msg(&msg, NULL, NULL);
+	if (err < 0) {
+		return err;
+	}
+
 	return 0;
 }
 
