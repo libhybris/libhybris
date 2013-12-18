2c0394167e564f6c16f7e91b85f503e2b081aed1
David Greaves <david@dgreaves.com>
2013-12-18 17:18:45 +0000

    Generate android-config.h in extract-headers.sh This will be used to hold configuration options set during the android build in GLOBAL_CFLAGS etc

--- utils/extract-headers.sh
+++ utils/extract-headers.sh
@@ -113,6 +113,35 @@ cat > $HEADERPATH/android-version.h << EOF
 #endif
 EOF
 
+cat > $HEADERPATH/android-config.h << EOF
+#ifndef HYBRIS_CONFIG_H_
+#define HYBRIS_CONFIG_H_
+
+/* When android is built for a specific device the build is
+   modified by BoardConfig.mk and possibly other mechanisms.
+   eg
+   device/samsung/i9305/BoardConfig.mk: 
+       COMMON_GLOBAL_CFLAGS += -DCAMERA_WITH_CITYID_PARAM
+   device/samsung/smdk4412-common/BoardCommonConfig.mk:
+       COMMON_GLOBAL_CFLAGS += -DEXYNOS4_ENHANCEMENTS
+
+   This file allows those global configurations, which are not
+   otherwise defined in the build headers, to be available in
+   hybris builds.
+
+   Typically it is generated at hardware adaptation time.
+
+   The CONFIG GOES HERE line can be used by automation to modify
+   this file.
+*/
+
+#include <android/android-version.h>
+
+/* CONFIG GOES HERE */
+
+#endif
+EOF
+
 extract_headers_to hardware \
     hardware/libhardware/include/hardware
 
