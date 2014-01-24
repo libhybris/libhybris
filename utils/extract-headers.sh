892c747c29ebc02527afd6e866b8ce596e2f8373
Adrian Negreanu <adrian.m.negreanu@intel.com>
2014-01-24 12:39:55 +0200

    add makefile for android headers
    Signed-off-by: Adrian Negreanu <adrian.m.negreanu@intel.com>

--- utils/extract-headers.sh
+++ utils/extract-headers.sh
@@ -230,6 +230,27 @@ if [ -e $ANDROID_ROOT/.repo/manifest.xml ]; then
     cp $ANDROID_ROOT/.repo/manifest.xml $HEADERPATH/
 fi
 
+# Add a makefile to make packaging easier
+cat > ${HEADERPATH}/Makefile << EOF
+PREFIX?=/usr/local
+INCLUDEDIR?=\$(PREFIX)/include/android
+all:
+	@echo "Use '\$(MAKE) install' to install"
+
+install:
+	mkdir -p \$(DESTDIR)/\$(INCLUDEDIR)
+	cp android-config.h android-version.h \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r hardware \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r hardware_legacy \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r cutils \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r system \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r android \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r linux \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r sync \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r libnfc-nxp \$(DESTDIR)/\$(INCLUDEDIR)
+	cp -r private \$(DESTDIR)/\$(INCLUDEDIR)
+EOF
+
 find "$HEADERPATH" -type f -exec chmod 0644 {} \;
 
 exit 0
