1849dd9dc1b8f7b9e89e9b71f21e04787641fad6
You-Sheng Yang <vicamo@gmail.com>
2015-11-19 16:36:40 +0800

    utils: fix header extraction for Android 6.0
    Signed-off-by: You-Sheng Yang <vicamo@gmail.com>

--- utils/extract-headers.sh
+++ utils/extract-headers.sh
@@ -183,6 +183,10 @@ if [ $MAJOR -ge 4 ]; then
     extract_headers_to system \
         system/core/include/system
 fi
+if [ $MAJOR -ge 6 ]; then
+    extract_headers_to system \
+        system/media/audio/include/system
+fi
 
 extract_headers_to android \
     system/core/include/android
@@ -194,7 +198,7 @@ if [ $MAJOR -eq 4 -a $MINOR -ge 1 ]; then
 
     extract_headers_to sync \
         system/core/include/sync
-elif [ $MAJOR -eq 5 ]; then
+elif [ $MAJOR -ge 5 ]; then
     extract_headers_to linux \
         bionic/libc/kernel/uapi/linux/sync.h \
         bionic/libc/kernel/uapi/linux/sw_sync.h
