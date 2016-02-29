4aeba8dade7aaa3392cb9ea1c49a13eab56e3f1e
Johan Herland <johan@herland.net>
2016-02-29 19:22:30 +0100

    utils/extract-headers.sh: Don't fail on PLATFORM_VERSION := 6.0
    It seems Android v6 has dropped patch level from the version number.
    Handle this by haveing extract-headers.sh default to patch level 0,
    if missing.

--- utils/extract-headers.sh
+++ utils/extract-headers.sh
@@ -46,8 +46,11 @@ if [ x$MAJOR = x -o x$MINOR = x -o x$PATCH = x ]; then
 $(IFS="." awk '/PLATFORM_VERSION := ([0-9.]+)/ { print $3; }' < $VERSION_DEFAULTS)
 EOF
 
-    if [ x$MAJOR = x -o x$MINOR = x -o x$PATCH = x ]; then
+    if [ x$MAJOR = x -o x$MINOR = x ]; then
         parse_defaults_failed
+    fi
+    if [ x$PATCH = x ]; then
+        PATCH=0
     fi
 
     echo -n "Auto-detected version: ${MAJOR}.${MINOR}.${PATCH}";echo "${PATCH2:+.${PATCH2}}${PATCH3:+.${PATCH3}}"
