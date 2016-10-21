e0d4381069c9877107815837bd71b10ae3b34ad7
Frajo Haider <f_haider@gmx.at>
2016-10-21 16:00:10 +0200

    [glesv1] let the linker decide the location of libGLESv1_CM.so

--- hybris/glesv1/glesv1_cm.c
+++ hybris/glesv1/glesv1_cm.c
@@ -26,9 +26,9 @@
 
 #include <hybris/common/binding.h>
 
-#define GLESV1_CM_LIBRARY_PATH "/system/lib/libGLESv1_CM.so"
+#define GLESV1_CM_LIBRARY "libGLESv1_CM.so"
 
-HYBRIS_LIBRARY_INITIALIZE(glesv1_cm, GLESV1_CM_LIBRARY_PATH);
+HYBRIS_LIBRARY_INITIALIZE(glesv1_cm, GLESV1_CM_LIBRARY);
 
 /* Scripts to generate these bindings can be found in utils/generate_glesv1/ */
 HYBRIS_IMPLEMENT_VOID_FUNCTION2(glesv1_cm, glAlphaFunc, GLenum, GLclampf);
