b3dcf80be9b030e8d3220444082e333e1f68c188
Ilya Bizyaev <bizyaev@zoho.com>
2018-01-14 16:15:57 +0300

    Fix TLS slots to be compatible with x86

--- hybris/common/mm/bionic/libc/private/bionic_tls.h
+++ hybris/common/mm/bionic/libc/private/bionic_tls.h
@@ -51,12 +51,17 @@ __BEGIN_DECLS
 enum {
   TLS_SLOT_SELF = 0, // The kernel requires this specific slot for x86.
   TLS_SLOT_THREAD_ID,
-  TLS_SLOT_ERRNO = 5,
+
+  TLS_SLOT_STACK_GUARD = 5, // GCC requires this specific slot for x86.
+
+  TLS_SLOT_ERRNO = 6,
 
   // These two aren't used by bionic itself, but allow the graphics code to
   // access TLS directly rather than using the pthread API.
-  TLS_SLOT_OPENGL_API = 6,
-  TLS_SLOT_OPENGL = 7,
+  TLS_SLOT_OPENGL_API = 7,
+  TLS_SLOT_OPENGL = 8,
+
+  TLS_SLOT_DLERROR = 9,
 
   // This slot is only used to pass information from the dynamic linker to
   // libc.so when the C library is loaded in to memory. The C runtime init
@@ -64,11 +69,9 @@ enum {
   // we reuse an existing location that isn't needed during libc startup.
   TLS_SLOT_BIONIC_PREINIT = TLS_SLOT_OPENGL_API,
 
-  TLS_SLOT_STACK_GUARD = 8, // GCC requires this specific slot for x86.
-  TLS_SLOT_DLERROR,
-
   BIONIC_TLS_SLOTS // Must come last!
 };
+
 
 /*
  * Bionic uses some pthread keys internally. All pthread keys used internally
