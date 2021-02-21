e0690a456b0384af910ab40653d946d113c1f973
Frajo Haider <f_haider@gmx.at>
2021-02-21 20:24:34 +0200

    hybris: fix wrappers for aarch64

--- hybris/common/wrapper_code.h
+++ hybris/common/wrapper_code.h
@@ -22,8 +22,11 @@
 extern "C" {
 #endif
 
-void wrapper_code_generic() __attribute__((naked,noinline)) __attribute__((target("arm")));
+#ifdef __aarch64__
+void wrapper_code_generic() __attribute__((naked,noinline));
+#endif
 #ifdef __arm__
+void wrapper_code_generic() __attribute__((naked,noinline)) __attribute__((target("arm")));
 void wrapper_code_generic_thumb() __attribute__((naked,noinline)) __attribute__((target("thumb")));
 #endif
 
