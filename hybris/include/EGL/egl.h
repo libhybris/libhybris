d31230d854c7041a48be64047b939b09dd23f580
Matti Lehtimäki <matti.lehtimaki@jolla.com>
2022-04-23 22:21:45 +0300

    hybris: include: Update EGL headers

--- hybris/include/EGL/egl.h
+++ hybris/include/EGL/egl.h
@@ -14,7 +14,7 @@ extern "C" {
 ** used to make the header, and the header can be found at
 **   http://www.khronos.org/registry/egl
 **
-** Khronos $Git commit SHA1: 5a9a7e3fcb $ on $Git commit date: 2020-08-24 11:05:32 -0700 $
+** Khronos $Git commit SHA1: 8c62b915dd $ on $Git commit date: 2021-11-05 23:32:01 -0400 $
 */
 
 #include <EGL/eglplatform.h>
@@ -23,7 +23,7 @@ extern "C" {
 #define EGL_EGL_PROTOTYPES 1
 #endif
 
-/* Generated on date 20201001 */
+/* Generated on date 20211116 */
 
 /* Generated C header for:
  * API: egl
